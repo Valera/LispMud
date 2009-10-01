60f4f767d4e541bfac826e85ff5a31b6efab79b5
Valeriy Fedotov <valeriy.fedotov@gmail.com>
2009-10-02 00:50:37 +0400

    Started working on registration process.

--- src/registration.lisp
+++ src/registration.lisp
@@ -9,18 +9,18 @@
 
 (defvar *user-db* (make-hash-table :test 'equal))
 
-(defun read-value-in-range (stream prompt lower upper)
+(defun read-value-in-range (prompt lower upper)
   "Reads integer value from stream from upper to lower inclusive"
-  (format stream "~a: " prompt)
-  (let ((val (read stream)))
+  (format t "~a: " prompt)
+  (let ((val (read)))
     (if (and (integerp val) (<= lower val upper))
 	val
-	(read-value-in-range stream prompt lower upper))))
+	(read-value-in-range prompt lower upper))))
 
-(defun prompt-read (prompt stream &key satisfy-p)
-  (format stream "~a: " prompt)
+(defun prompt-read (prompt &key satisfy-p)
+  (format t "~a: " prompt)
   (loop
-       for value = (read-line stream) then (read-line stream)
+       for value = (read-line) then (read-line)
        when (or (not satisfy-p) (funcall satisfy-p value))
        do (return value)))
 
@@ -56,3 +56,40 @@
 
 (defun print-hash (hash)
   (maphash #'(lambda (key val) (format t "~a => ~a~%" key val)) hash))
+
+;;; FIXME - end - leak
+(defmacro deffsm (name &body body)
+  (let ((vars (mapcar #'second body)))
+    `(defun ,name ()
+       (let ,vars
+	 (tagbody
+	    ,@(loop for state-descr in body
+		   nconc (destructuring-bind (go-state var read-fun dispatch-fun) state-descr
+			   (list go-state
+				 `(setf ,var ,read-fun)
+				 dispatch-fun)))
+	    )))))
+
+(defun can-login (a b) nil)
+
+(deffsm enter-game
+  (go-enter enter (read-value-in-range *first-prompt* 1 2)
+	    (ecase enter
+	      (1 (go go-login-name))
+	      (2 (go go-register-name))))
+  (go-register-name rname (prompt-read "Введите имя")
+		    (go go-register-pass1))
+  (go-register-pass1 rpass1 (prompt-read "Введите пароль")
+		     (go go-register-pass2))
+  (go-register-pass2 rpass2 (prompt-read "Введите пароль ещё раз")
+		     (if (string= rpass1 rpass2)
+			 (progn (format t "Регистрация завершена.~%")
+				(return-from enter-game))
+			 (progn (format t "Пароли не совпадают. Пожалуйста, пройдите регистрацию заново.~%")
+				(go go-register-name))))
+  (go-login-name lname (prompt-read "Имя")
+		 (go go-login-pass))
+  (go-login-pass lpass (prompt-read "Пароль")
+		 (if (can-login lname lpass)
+		     (login lname lpass)
+		     (go go-login-name))))
